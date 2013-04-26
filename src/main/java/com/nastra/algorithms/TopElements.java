282dadf361e500150d9b6014fa3d9c729fef2957
Eduard Tudenhoefner <etudenhoefner@googlemail.com>
2013-04-26 19:19:30 +0200

    Added algorithm to retrieve the Top K elements from a list

---
+++ src/main/java/com/nastra/algorithms/TopElements.java
@@ -0,0 +1,64 @@
+package com.nastra.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Retrieves the top k elements in increasing order from a given list in an efficient way by using a priority queue. Its running time is O(n log k). If the top k elements are not required to be in
+ * sorted order, then we use a selection-based approach.
+ *
+ * @author nastra - Eduard Tudenhoefner
+ */
+public class TopElements {
+
+    public static <T extends Comparable> List<T> findTopElements(List<T> unsortedInput, int topElements, boolean sortTopElements) {
+        if (sortTopElements) {
+            return getSortedTopElements(unsortedInput, topElements);
+        }
+        return getUnsortedTopElements(unsortedInput, topElements);
+    }
+
+    /**
+     * We use an approach based on a binary heap (priority queue) to retrieve the top k elements in sorted order.
+     *
+     * @param <T>
+     * @param unsortedInput
+     * @param topElements
+     * @return
+     */
+    private static <T extends Comparable> List<T> getSortedTopElements(List<T> unsortedInput, int topElements) {
+        List<T> result = new ArrayList<T>();
+        PriorityQueue<T> queue = new PriorityQueue<T>(topElements);
+        for (T item : unsortedInput) {
+            if (queue.size() < topElements) {
+                queue.add(item);
+            } else if (lessThan(queue.peek(), item)) {
+                queue.poll();
+                queue.add(item);
+            }
+        }
+
+        for (int i = 0; i < topElements; i++) {
+            result.add(queue.poll());
+        }
+
+        return result;
+    }
+
+    /**
+     * We use a selection-based approach here to retrieve the top k elements.
+     *
+     * @param <T>
+     * @param unsortedInput
+     * @param topElements
+     * @return
+     */
+    private static <T extends Comparable> List<T> getUnsortedTopElements(List<T> unsortedInput, int topElements) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    private static boolean lessThan(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/TopElementsTest.java
@@ -0,0 +1,48 @@
+package com.nastra.algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class TopElementsTest {
+
+    @Test
+    public void testTopElements() {
+        Random random = new Random();
+        List<Integer> items = new ArrayList<Integer>();
+        List<Integer> sorted = new ArrayList<Integer>();
+        int topElements = 100;
+        for (int i = 0; i < 100000; i++) {
+            Integer value = random.nextInt();
+            items.add(value);
+            sorted.add(value);
+        }
+
+        List<Integer> top = TopElements.findTopElements(items, topElements, true);
+        List<Integer> expectedTop = retrieveTopElementsFromList(sorted, topElements);
+        Assert.assertTrue(top.size() == expectedTop.size());
+
+        for (int i = 0; i < topElements; i++) {
+            Assert.assertTrue(expectedTop.get(i).equals(top.get(i)));
+        }
+    }
+
+    private List<Integer> retrieveTopElementsFromList(List<Integer> input, int topElements) {
+        Collections.sort(input);
+        Collections.reverse(input);
+        List<Integer> result = new ArrayList<Integer>(topElements);
+        for (int i = 0; i < topElements; i++) {
+            result.add(input.get(i));
+        }
+        // so that we get the top elements in increasing order
+        Collections.reverse(result);
+        return result;
+    }
+}
