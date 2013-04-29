b762c31db53342d3cea20ce6538f561ccfae31d8
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2013-04-29 21:12:24 +0200

    Added a rudimentary infix-to-postfix translator

---
+++ src/main/java/com/nastra/algorithms/InfixToPostfixTranslator.java
@@ -0,0 +1,97 @@
+package com.nastra.algorithms;
+
+import java.util.Stack;
+
+/**
+ * A rudimentary implementation to translate from infix to postfix expressions. Currently the input is not checked for correctness and only the following things are supported for now:
+ * <br>- single digit operands are supported
+ * <br>- The operators
+ *
+ * @author nastra
+ */
+public class InfixToPostfixTranslator {
+
+    private static final String OPERAND_REGEX = "[A-Za-z0-9]";
+    private static final char LEFT_PARENTHESIS = '(';
+    private static final char RIGHT_PARENTHESIS = ')';
+
+    public static String translate(String in) {
+        if (null == in || in.isEmpty()) {
+            return in;
+        }
+        StringBuilder out = new StringBuilder();
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < in.length(); i++) {
+            char current = in.charAt(i);
+            if (isOperand(current)) {
+                out.append(current);
+            } else if (isLeftParenthesis(current) || isOperator(current)) {
+                if (isOperator(current) && higherOrEqualOperatorOnStack(stack, current) && !isRightParenthesis(current)) {
+                    out.append(stack.pop());
+                }
+                stack.push(current);
+            } else if (isRightParenthesis(current)) {
+                while (!isLeftParenthesis(stack.peek())) {
+                    out.append(stack.pop());
+                }
+                stack.pop(); // pop the remaining '('
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            out.append(stack.pop());
+        }
+
+        return out.toString();
+    }
+
+    private static boolean isOperand(char current) {
+        return Character.valueOf(current).toString().matches(OPERAND_REGEX);
+    }
+
+    private static boolean isLeftParenthesis(char current) {
+        return LEFT_PARENTHESIS == current;
+    }
+
+    private static boolean isRightParenthesis(char current) {
+        return RIGHT_PARENTHESIS == current;
+    }
+
+    private static boolean isOperator(char current) {
+        return Operator.PLUS.operator == current || Operator.MINUS.operator == current || Operator.MULTIPLY.operator == current || Operator.DIVIDE.operator == current;
+    }
+
+    private static boolean higherOrEqualOperatorOnStack(Stack<Character> stack, char current) {
+        return !stack.isEmpty() && (precedenceOf(stack.peek()) >= precedenceOf(current));
+    }
+
+    private static int precedenceOf(char operator) {
+        if (operator == Operator.PLUS.operator || operator == Operator.MINUS.operator) {
+            return Operator.PLUS.precedence;
+        } else if (operator == Operator.MULTIPLY.operator || operator == Operator.DIVIDE.operator) {
+            return Operator.MULTIPLY.precedence;
+        }
+        return 0;
+    }
+
+    private static enum Operator {
+
+        PLUS('+', 1), MINUS('-', 1), MULTIPLY('*', 2), DIVIDE('/', 2);
+        private char operator;
+        private int precedence;
+
+        private Operator(char operator, int precedence) {
+            this.operator = operator;
+            this.precedence = precedence;
+        }
+
+        public char getOperator() {
+            return operator;
+        }
+
+        public int getPrecedence() {
+            return precedence;
+        }
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/InfixToPostfixTranslatorTest.java
@@ -0,0 +1,34 @@
+package com.nastra.algorithms;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class InfixToPostfixTranslatorTest {
+
+    @Test
+    public void testInfixToPostfixTranslation() {
+        String infix = "A*B-(C*D)+E";
+        String expectedPostfix = "AB*CD*-E+";
+        String postfix = InfixToPostfixTranslator.translate(infix);
+        Assert.assertEquals(infix + " could not be resolved to postfix. The result was: " + postfix, expectedPostfix, postfix);
+
+        infix = "A*B-(C*D)*E";
+        expectedPostfix = "AB*CD*E*-";
+        postfix = InfixToPostfixTranslator.translate(infix);
+        Assert.assertEquals(infix + " could not be resolved to postfix. The result was: " + postfix, expectedPostfix, postfix);
+
+        infix = "2+(3*4)-6";
+        expectedPostfix = "234*+6-";
+        postfix = InfixToPostfixTranslator.translate(infix);
+        Assert.assertEquals(infix + " could not be resolved to postfix. The result was: " + postfix, expectedPostfix, postfix);
+
+        infix = "(A-B)*(C-D)-E*F";
+        expectedPostfix = "AB-CD-*EF*-";
+        postfix = InfixToPostfixTranslator.translate(infix);
+        Assert.assertEquals(infix + " could not be resolved to postfix. The result was: " + postfix, expectedPostfix, postfix);
+    }
+}
