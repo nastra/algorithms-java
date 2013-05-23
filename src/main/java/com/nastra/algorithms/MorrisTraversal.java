fb9ba6b1e6115b01bfd9ffb3d5cb632070d09aed
nastra <etudenhoefner@gmail.com>
2013-05-23 09:39:29 +0200

    Added morris traversal algorithm

---
+++ src/main/java/com/nastra/algorithms/MorrisTraversal.java
@@ -0,0 +1,58 @@
+package com.nastra.algorithms;
+
+import java.util.List;
+
+/**
+ *
+ * @author nastra - Eduard Tudenhoefner
+ */
+public class MorrisTraversal<T> {
+
+    private Node root;
+
+    private Node getMax(Node node) {
+        Node current = node;
+        while (null != current && null != current.right) {
+            current = current.right;
+        }
+        return current;
+    }
+
+    private Node<T> getParentOf(Node<T> temp, Node left) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private class Node<T> {
+
+        T value;
+        Node left;
+        Node right;
+    }
+
+    /**
+     * Traverses a tree without using recursion or any other helper data structures
+     *
+     * @param root The root of the tree
+     * @param values The list where to store the traversed values of the tree.
+     */
+    public void traverse(Node root, List<T> values) {
+        Node<T> current = root;
+        while (null != current) {
+            if (null != current.left) {
+                Node<T> temp = getMax(current.left);
+                if (temp.equals(current)) {
+                    // restore the tree
+                    Node<T> p = getParentOf(temp, current.left);
+                    p.right = null;
+                    current = current.right;
+                } else {
+                    temp.right = current;
+                    current = current.left;
+                }
+            } else {
+                values.add(current.value);
+                current = current.right;
+            }
+        }
+    }
+}
