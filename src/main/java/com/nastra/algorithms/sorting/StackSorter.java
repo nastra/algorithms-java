88023fcc7f1d0a6f59506be12e0b33678c24c93c
nastra <etudenhoefner@gmail.com>
2013-10-11 22:28:05 +0200

    Added solution to sort a stack without additional data structures

---
+++ src/main/java/com/nastra/algorithms/sorting/StackSorter.java
@@ -0,0 +1,38 @@
+package com.nastra.algorithms.sorting;
+
+import java.util.Stack;
+
+/**
+ * Solution to the following interview question: "Sort a stack of numbers in descending order. The following operations are allowed:
+ * push/pop/top/isEmpty."
+ *
+ * @author nastra
+ */
+public class StackSorter {
+
+    /**
+     * Sorts a stack without using additional data structures. Requires O(n^2) time and O(n) space.
+     *
+     * @param stack The stack to be sorted
+     */
+    public static void sort(Stack<Integer> stack) {
+        if (null == stack) {
+            return;
+        }
+        if (!stack.isEmpty()) {
+            Integer val = stack.pop();
+            sort(stack);
+            insert(stack, val);
+        }
+    }
+
+    private static void insert(Stack<Integer> stack, Integer val) {
+        if (stack.isEmpty() || stack.peek() <= val) {
+            stack.push(val);
+        } else {
+            Integer x = stack.pop();
+            insert(stack, val);
+            stack.push(x);
+        }
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/StackSorterTest.java
@@ -0,0 +1,32 @@
+package com.nastra.algorithms;
+
+import com.nastra.algorithms.sorting.StackSorter;
+import java.util.Random;
+import java.util.Stack;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class StackSorterTest {
+
+    @Test
+    public void testStackSorting() {
+        Random random = new Random();
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < 1000; i++) {
+            stack.push(random.nextInt(10000));
+        }
+        StackSorter.sort(stack);
+
+        Integer prev = null;
+        for (Integer val : stack) {
+            if (null != prev) {
+                Assert.assertTrue(prev <= val, "The previous value '" + prev + "' was smaller than the current value '" + val + "'!");
+            }
+            prev = val;
+        }
+    }
+}
