ef13919c2ced0b849440db83ac080f3b234e6a68
nastra <etudenhoefner@gmail.com>
2013-10-13 19:43:24 +0200

    minor improvement on the KeyValueList data structure

--- src/main/java/com/nastra/datastructures/KeyValueList.java
+++ src/main/java/com/nastra/datastructures/KeyValueList.java
@@ -1,19 +1,28 @@
 package com.nastra.datastructures;
 
 /**
+ * A singly linked list where each node consists of a <Key, Value> pair.
  *
- * @author nastra
+ * @author nastra - Eduard Tudenhoefner
  */
 public class KeyValueList<K, V> {
 
     private KeyValueNode<K, V> head;
+    private KeyValueNode<K, V> tail;
 
     public KeyValueList() {
     }
 
+    /**
+     * Inserting a new element is done in time O(n) in the worst case.
+     *
+     * @param key
+     * @param value
+     */
     public void put(K key, V value) {
         if (null == head) {
             head = new KeyValueNode(key, value);
+            tail = head;
             return;
         }
 
@@ -22,12 +31,8 @@ public class KeyValueList<K, V> {
             // update the value
             node.setValue(value);
         } else {
-            node = new KeyValueNode(key, value);
-            KeyValueNode<K, V> n = head;
-            while (null != n.getNext()) {
-                n.setNext(n.getNext());
-            }
-            n.setNext(node);
+            tail.setNext(new KeyValueNode(key, value));
+            tail.setNext(tail.getNext());
         }
     }
 
@@ -41,6 +46,12 @@ public class KeyValueList<K, V> {
         return null;
     }
 
+    /**
+     * Retrieving a node is done in time O(n) in the worst case.
+     *
+     * @param key The key to search for
+     * @return The value associated with the given key.
+     */
     public V get(K key) {
         KeyValueNode<K, V> node = getNode(key);
         if (null != node) {
