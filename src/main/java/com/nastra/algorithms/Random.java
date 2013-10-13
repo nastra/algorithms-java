8aa519efe6546c6221710e26651fb6b7e692be2a
nastra <etudenhoefner@gmail.com>
2013-10-13 15:07:16 +0200

    Added random generator with test

---
+++ src/main/java/com/nastra/algorithms/Random.java
@@ -0,0 +1,19 @@
+package com.nastra.algorithms;
+
+/**
+ *
+ * @author nastra
+ */
+public class Random {
+
+    private static java.util.Random random = new java.util.Random();
+
+    public static int randInt(int min, int max) {
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = random.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/RandomTest.java
@@ -0,0 +1,54 @@
+package com.nastra.algorithms;
+
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class RandomTest {
+
+    @Test
+    public void testSimple() {
+        int min = 5;
+        int max = 6;
+        verify(min, max);
+    }
+
+    @Test
+    public void testSmallRange() {
+        int min = 0;
+        int max = 10;
+        verify(min, max);
+    }
+
+    @Test
+    public void testBigRange() {
+        int min = 37;
+        int max = 10123902;
+        verify(min, max);
+    }
+
+    @Test
+    public void testRepeatedly() {
+        int min = 0;
+        int max = 1000;
+        for (int i = 0; i < 5000; i++) {
+            verify(min, max);
+        }
+    }
+
+    @Test
+    public void testNegative() {
+        int min = -5;
+        int max = -1;
+        verify(min, max);
+    }
+
+    private void verify(int min, int max) {
+        int result = Random.randInt(min, max);
+        Assert.assertTrue(min <= result && result <= max);
+
+    }
+}
