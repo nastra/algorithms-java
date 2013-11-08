82620c2abfabe9779b5c9c857b861140f3d54b97
nastra <etudenhoefner@gmail.com>
2013-11-08 22:37:53 +0100

    added O(n) solution to finding the ith order statistic

---
+++ src/main/java/com/nastra/algorithms/search/OrderStatistic.java
@@ -0,0 +1,68 @@
+package com.nastra.algorithms.search;
+
+import com.nastra.algorithms.Random;
+
+/**
+ * This program determines the kth order statistic (the kth smallest number in a list) in O(n) time in the average case and O(n^2) time in the worst
+ * case. It achieves this through the Quickselect algorithm.
+ *
+ * @author nastra - Eduard Tudenhoefner
+ */
+public class OrderStatistic {
+
+    public static Integer select(int[] a, int k) {
+        if (null == a || a.length == 0) {
+            return null;
+        }
+
+        if (k < 1 || k > a.length) {
+            return null;
+        }
+
+        return select(a, k, 0, a.length - 1);
+    }
+
+    private static Integer select(int[] a, int k, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        if (low == high) {
+            return a[low];
+        }
+        int pivotIndex = randomizedPartition(a, low, high);
+        int leftSize = pivotIndex - low + 1;
+
+        if (leftSize == k) {
+            return a[pivotIndex];
+        } else if (k < leftSize) {
+            return select(a, k, low, pivotIndex - 1);
+        } else {
+            return select(a, k - leftSize, pivotIndex + 1, high);
+        }
+    }
+
+    private static int randomizedPartition(int[] in, int low, int high) {
+        int randomIndex = Random.randInt(low, high);
+        swap(in, low, randomIndex);
+        return partition(in, low, high);
+    }
+
+    private static int partition(int[] a, int low, int high) {
+        int p = low;
+        int i = low + 1;
+        for (int j = low + 1; j <= high; j++) {
+            if (a[j] <= a[p]) {
+                swap(a, i, j);
+                i++;
+            }
+        }
+        swap(a, low, i - 1);
+        return i - 1;
+    }
+
+    private static void swap(int[] in, int i, int j) {
+        int tmp = in[i];
+        in[i] = in[j];
+        in[j] = tmp;
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/OrderStatisticTest.java
@@ -0,0 +1,83 @@
+package com.nastra.algorithms;
+
+import com.nastra.algorithms.search.OrderStatistic;
+import java.util.Arrays;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class OrderStatisticTest {
+
+    @Test
+    public void testNull() {
+        int k = 3;
+        Integer result = OrderStatistic.select(null, k);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testOneValue() {
+        int[] a = {4};
+        int k = 1;
+        Integer result = OrderStatistic.select(a, k);
+        Assert.assertTrue(result.equals(4));
+    }
+
+    @Test
+    public void testTwoValues() {
+        int[] a = {4, 9};
+        int k = 1;
+        Integer result = OrderStatistic.select(a, k);
+        Assert.assertTrue(result.equals(4));
+    }
+
+    @Test
+    public void testKOutOfBounds() {
+        int[] a = {4, 9, 12};
+        int k = 0;
+        Integer result = OrderStatistic.select(a, k);
+        Assert.assertNull(result);
+        k = 4;
+        result = OrderStatistic.select(a, k);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testSmallArray() {
+        int[] a = {4, 9, 3, 6, 8, 1, 7};
+        int k = 4;
+        Integer result = OrderStatistic.select(a, k);
+        Assert.assertTrue(result.equals(6));
+    }
+
+    @Test
+    public void testSmallArrayTwo() {
+    }
+
+    @Test
+    public void testLargeRandomArray() {
+        int[] sortedArray = generateArrayWithRandomNumbers();
+        int[] a = sortedArray.clone();
+        Arrays.sort(sortedArray);
+
+        for (int i = 0; i < 10000; i++) {
+            int randomIndex = Random.randInt(0, sortedArray.length - 1);
+            Integer element = sortedArray[randomIndex];
+            Integer result = OrderStatistic.select(a, randomIndex + 1);
+            Assert.assertEquals(element, result);
+        }
+    }
+
+    private int[] generateArrayWithRandomNumbers() {
+        java.util.Random random = new java.util.Random();
+        int[] array = new int[random.nextInt(1000)];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+//        Arrays.sort(array);
+        return array;
+    }
+}
