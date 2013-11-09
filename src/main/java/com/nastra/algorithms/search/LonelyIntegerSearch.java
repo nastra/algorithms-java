a3ffe3456c50e53cde7b799a6b6f05c194a3a910
nastra <etudenhoefner@gmail.com>
2013-11-09 22:19:33 +0100

    added solution to the lonely integer problem

---
+++ src/main/java/com/nastra/algorithms/search/LonelyIntegerSearch.java
@@ -0,0 +1,20 @@
+package com.nastra.algorithms.search;
+
+/**
+ * There are N integers in an array A. All but one integer occurs in pairs. Your task is to find out that number that occurs only once.
+ *
+ * @author nastra
+ */
+public class LonelyIntegerSearch {
+
+    public static Integer findNumber(int[] s) {
+        if (null == s || s.length == 0) {
+            return null;
+        }
+        int result = 0;
+        for (int value : s) {
+            result ^= value;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/nastra/algorithms/LonelyIntegerSearchTest.java
@@ -0,0 +1,48 @@
+package com.nastra.algorithms;
+
+import com.nastra.algorithms.search.LonelyIntegerSearch;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author nastra
+ */
+public class LonelyIntegerSearchTest {
+
+    @Test
+    public void testNull() {
+        int[] s = null;
+        Integer result = LonelyIntegerSearch.findNumber(s);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testOne() {
+        int[] s = {1};
+        Integer result = LonelyIntegerSearch.findNumber(s);
+        Assert.assertEquals(result, Integer.valueOf(1));
+    }
+
+    @Test
+    public void testTwo() {
+        int[] s = {2, 2};
+        Integer result = LonelyIntegerSearch.findNumber(s);
+        Assert.assertEquals(result, Integer.valueOf(0));
+    }
+
+    @Test
+    public void testThree() {
+        int[] s = {1, 1, 2};
+        Integer result = LonelyIntegerSearch.findNumber(s);
+        Assert.assertEquals(result, Integer.valueOf(2));
+    }
+
+    @Test
+    public void testSmall() {
+        int[] s = {0, 0, 1, 2, 1};
+        Integer result = LonelyIntegerSearch.findNumber(s);
+        Assert.assertEquals(result, Integer.valueOf(2));
+
+    }
+}
