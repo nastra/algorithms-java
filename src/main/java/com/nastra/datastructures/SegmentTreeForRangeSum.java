f0d51eecdf6359299586e816e764e285aa790c6d
Eduard Tudenhöfner <etudenhoefner@gmail.com>
2014-08-14 16:25:46 +0200

    updated segment tree for range sum

--- src/main/java/com/nastra/datastructures/SegmentTreeForRangeSum.java
+++ src/main/java/com/nastra/datastructures/SegmentTreeForRangeSum.java
@@ -6,22 +6,25 @@ import java.util.Arrays;
  * A simple segment tree that allows the following two operations in time O(log n):<br>
  * <li>Find the sum of elements from index l to r where 0 <= l <= r <= n-1</li><br>
  * <li>Change value of a specified element of the array arr[i] = x where 0 <= i <= n-1.</li><br>
+ * <br>
+ * The height of the tree is ceil(log(n)) where n is the number of elements that should be stored in the tree. The maximal size for storing internal
+ * nodes and leaves is then 2*2^height-1.
  * 
  * @author nastra
  * 
  */
-public class SegmentTree {
+public class SegmentTreeForRangeSum {
     private int[] tree;
     private int n;
     private int startSeg;
     private int endSeg;
     private int root = 0;
 
-    public SegmentTree(int[] a) {
+    public SegmentTreeForRangeSum(int[] a) {
         n = a.length;
         int height = (int) (Math.ceil(Math.log(n) / Math.log(2)));
-        int maxsize = 2 * (int) Math.pow(2, height) - 1;
-        tree = new int[maxsize];
+        int maxSize = 2 * (int) Math.pow(2, height) - 1;
+        tree = new int[maxSize];
         startSeg = 0;
         endSeg = n - 1;
         constructTree(a, startSeg, endSeg, root);
@@ -49,14 +52,14 @@ public class SegmentTree {
         tree[index] = tree[index] + update;
         if (segmentStart != segmentEnd) {
             int mid = mid(segmentStart, segmentEnd);
-            updateTreeUtil(segmentStart, mid, updateAt, update, index * 2 + 1);
-            updateTreeUtil(mid + 1, segmentEnd, updateAt, update, index * 2 + 2);
+            updateTreeUtil(segmentStart, mid, updateAt, update, leftChild(index));
+            updateTreeUtil(mid + 1, segmentEnd, updateAt, update, rightChild(index));
         }
     }
 
     public int getSum(int queryStart, int queryEnd) {
         // Check for erroneous input values
-        if (queryStart < 0 || queryEnd > n - 1 || queryStart > queryEnd) {
+        if (queryStart < startSeg || queryEnd > endSeg || queryStart > queryEnd) {
             return -1;
         }
 
@@ -65,7 +68,7 @@ public class SegmentTree {
 
     private int getSum(int segmentStart, int segmentEnd, int queryStart, int queryEnd, int index) {
         // If segment of this node is a part of given range, then return the
-        // sum of the segment
+        // sum of the segment -> range of node is within queryStart and queryEnd
         if (queryStart <= segmentStart && queryEnd >= segmentEnd)
             return tree[index];
 
@@ -74,7 +77,16 @@ public class SegmentTree {
             return 0;
 
         int mid = mid(segmentStart, segmentEnd);
-        return getSum(segmentStart, mid, queryStart, queryEnd, index * 2 + 1) + getSum(mid + 1, segmentEnd, queryStart, queryEnd, index * 2 + 2);
+        return getSum(segmentStart, mid, queryStart, queryEnd, leftChild(index))
+                + getSum(mid + 1, segmentEnd, queryStart, queryEnd, rightChild(index));
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
     }
 
     private int constructTree(int[] a, int low, int high, int index) {
@@ -85,18 +97,18 @@ public class SegmentTree {
             return a[low];
         }
         int mid = mid(low, high);
-        if (index * 2 + 1 < tree.length) {
-            tree[index] += constructTree(a, low, mid, index * 2 + 1);
+        if (leftChild(index) < tree.length) {
+            tree[index] += constructTree(a, low, mid, leftChild(index));
         }
-        if (index * 2 + 2 < tree.length) {
-            tree[index] += constructTree(a, mid + 1, high, index * 2 + 2);
+        if (rightChild(index) < tree.length) {
+            tree[index] += constructTree(a, mid + 1, high, rightChild(index));
         }
         return tree[index];
     }
 
     public static void main(String[] args) {
         int arr[] = {2, 6, 9, 1, 3, 5};
-        SegmentTree t = new SegmentTree(arr);
+        SegmentTreeForRangeSum t = new SegmentTreeForRangeSum(arr);
         System.out.println(Arrays.toString(t.tree));
         System.out.println(Arrays.toString(arr));
         System.out.println(t.getSum(1, 3));
