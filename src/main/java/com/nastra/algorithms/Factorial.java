576c0c489aa26186c6bae65db9cb91e2522f6a36
Eduard Tudenhoefner <eduard.tudenhoefner@datastax.com>
2015-01-19 20:17:05 +0100

    added method to calcluate number of trailing zeros for a factorial

--- src/main/java/com/nastra/algorithms/Factorial.java
+++ src/main/java/com/nastra/algorithms/Factorial.java
@@ -12,4 +12,18 @@ public class Factorial {
         }
         return n * factorial(n - 1);
     }
+    
+    public static long numberOfTrailingZeros(int n) {
+    	long result = 0;
+		long divider = 5;
+		double res = n / divider;
+		result += (long) res;
+		while (res >= 1.0) {
+			divider = divider * 5;
+			res = n / divider;
+			result += (long) res;
+		}
+
+		return result;
+    }
 }
